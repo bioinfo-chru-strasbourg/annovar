9074537fc243adb4bfc06f96036d46b927019026
bioinfo-chru-strasbourg <40687400+bioinfo-chru-strasbourg@users.noreply.github.com>
2018-06-29 10:30:41 +0200

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,11 @@
+# ANNOVAR Documentation
+
+ANNOVAR is an efficient software tool to utilize update-to-date information to functionally annotate genetic variants detected from diverse genomes (including human genome hg18, hg19, hg38, as well as mouse, worm, fly, yeast and many others).
+
+This is the GitHub repository for the documentation of the ANNOVAR software, described in the paper listed below. Any edit to this repository will be reflected at ANNOVAR home page at http://annovar.openbioinformatics.org instantly.
+
+If you like this repository, please click on the "Star" button on top of this page, to show appreciation to the repository maintainer. If you want to receive notifications on changes to this repository, please click the "Watch" button on top of this page.
+
+## Reference
+
+- Wang K, Li M, Hakonarson H. [ANNOVAR: Functional annotation of genetic variants from next-generation sequencing data](http://nar.oxfordjournals.org/content/38/16/e164) _Nucleic Acids Research_, 38:e164, 2010
